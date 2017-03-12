bab1373ebe70005eb616eab206ef7d1cf09d0c87
Matteo Cardellini <matteo.cardellini@smileapp.it>
2017-03-12 14:59:16 +0100

    Added tabs for rapidly swiping between stocks

---
+++ app/src/main/java/com/udacity/stockhawk/adapters/StocksPagerAdapter.java
@@ -0,0 +1,48 @@
+package com.udacity.stockhawk.adapters;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import com.udacity.stockhawk.R;
+import com.udacity.stockhawk.ui.StockDetailFragment;
+
+/**
+ * Created by carde on 12/03/17.
+ */
+
+public class StocksPagerAdapter extends FragmentStatePagerAdapter {
+
+    private String[] stocks;
+    private FragmentManager fm;
+
+    public StocksPagerAdapter(FragmentManager fm, String[] stocks) {
+        super(fm);
+        this.stocks = stocks;
+        this.fm = fm;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        StockDetailFragment fragment = new StockDetailFragment();
+
+        Bundle args = new Bundle();
+        args.putString("SYMBOL", stocks[position]);
+
+        fragment.setArguments(args);
+
+        return fragment;
+    }
+
+    @Override
+    public int getCount() {
+        return stocks.length;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return stocks[position];
+    }
+}
--- app/src/main/java/com/udacity/stockhawk/ui/StockDetailActivity.java
+++ app/src/main/java/com/udacity/stockhawk/ui/StockDetailActivity.java
@@ -1,36 +1,94 @@
 package com.udacity.stockhawk.ui;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.LoaderManager;
 import android.support.v4.app.NavUtils;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 import com.udacity.stockhawk.R;
+import com.udacity.stockhawk.adapters.StocksPagerAdapter;
+import com.udacity.stockhawk.data.Contract;
 
-import timber.log.Timber;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
-public class StockDetailActivity extends AppCompatActivity {
+public class StockDetailActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    private StocksPagerAdapter mPagerAdapter;
+    private String mSymbol;
+    private static final int LOADER_ID = 534;
+
+    @BindView(R.id.detail_toolbar)
+    Toolbar mToolbar;
+
+    @BindView(R.id.detail_tabs)
+    TabLayout mTabs;
+
+    @BindView(R.id.detail_pager)
+    ViewPager mViewPager;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stock_detail);
+        ButterKnife.bind(this);
 
-
+        setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        Intent passedIntent = getIntent();
+        mViewPager.setAdapter(null);
 
-        StockDetailFragment fragment = new StockDetailFragment();
-        fragment.setArguments(passedIntent.getExtras());
+        mTabs.setupWithViewPager(mViewPager);
 
-        getSupportFragmentManager()
-                .beginTransaction()
-                .add(R.id.stock_detail_fragment, fragment)
-                .commit();
+
+        if (getIntent() != null && getIntent().hasExtra("SYMBOL")) {
+            mSymbol = getIntent().getStringExtra("SYMBOL");
+        }
+
+        getSupportLoaderManager().initLoader(LOADER_ID, null, this);
+
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return new CursorLoader(this,
+                Contract.Quote.URI,
+                Contract.Quote.QUOTE_COLUMNS.toArray(new String[]{}),
+                null, null, Contract.Quote.COLUMN_SYMBOL);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+
+        String[] stocks = new String[data.getCount()];
+        int selectedPage = -1;
+        for (int i = 0; i < data.getCount(); i++) {
+            data.moveToPosition(i);
+            String stock = data.getString(Contract.Quote.POSITION_SYMBOL);
+            stocks[i] = stock;
+            selectedPage = stock.equals(mSymbol) ? i : selectedPage;
+        }
+
+        mPagerAdapter = new StocksPagerAdapter(getSupportFragmentManager(), stocks);
+        mViewPager.setAdapter(mPagerAdapter);
+        if (selectedPage > -1) {
+            mViewPager.setCurrentItem(selectedPage);
+        }
+    }
+
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
 
     }
 
