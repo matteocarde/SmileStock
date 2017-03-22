cedc013498987edddb21a2c569c69d4752985fed
Matteo Cardellini <matteo.cardellini@smileapp.it>
2017-03-22 23:21:38 +0100

    Code review: added forgotten paddingStart and paddingEnd for RTL support

--- app/src/main/java/com/udacity/stockhawk/ui/MainActivity.java
+++ app/src/main/java/com/udacity/stockhawk/ui/MainActivity.java
@@ -2,7 +2,6 @@ package com.udacity.stockhawk.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
---
+++ app/src/test/java/com/udacity/stockhawk/utils/StocksUtilsTest.java
@@ -0,0 +1,12 @@
+package com.udacity.stockhawk.utils;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by carde on 17/03/17.
+ */
+public class StocksUtilsTest {
+
+
+
+}
